611b7814749aa2e94df6aada0e00f26da42e4aae
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-09-29 23:18:33 +0200

    1.3

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Main() {
     return (
-        <div>Main</div>
+        <div className='container'>
+            <img className='img-hero' src="https://res.cloudinary.com/dh4iesekp/image/upload/v1664485870/weather_hal3hg.png"/>
+            <div className='header-container'>
+                <h1>WheatSweet</h1>
+            </div>
+            <div className='hero-container'>
+                <h3>Date 29/09</h3>
+                <h1>Copenhagen</h1>
+                <h2>7°</h2>
+                <input type="search" placeholder='Search City'></input>
+            </div>
+        </div>
     )
 }
 
