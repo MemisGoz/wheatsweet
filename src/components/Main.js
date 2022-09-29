ea7b8b06818a3be5bf01ef3b9682eed1171bd633
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2022-09-30 01:56:30 +0200

    1.4

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Main() {
     return (
-        <div className='container'>
-            <img className='img-hero' src="https://res.cloudinary.com/dh4iesekp/image/upload/v1664485870/weather_hal3hg.png"/>
+        <div className='overlay'>
+        <div className='container'> 
             <div className='header-container'>
-                <h1>WheatSweet</h1>
+                <h4>WheatSweet</h4>
+                <h4>Made By Mgoz</h4>
             </div>
             <div className='hero-container'>
                 <h3>Date 29/09</h3>
@@ -13,6 +14,21 @@ function Main() {
                 <h2>7°</h2>
                 <input type="search" placeholder='Search City'></input>
             </div>
+            <div className='info-container'>
+                <div>
+                    <h4>Min Temp</h4>
+                    <h3>3°</h3>
+                </div>
+                <div>
+                <   h4>Max Temp</h4>
+                    <h3>9°</h3>
+                </div>
+                <div>
+                    <h4>What it feels like</h4>
+                    <h3>8°</h3>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
